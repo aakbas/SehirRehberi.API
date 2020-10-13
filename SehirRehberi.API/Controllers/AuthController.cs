849c32185d027ec944ad2d657cbc6a2671a15caa
Abdullah Akbaş <39574496+aakbas@users.noreply.github.com>
2020-10-13 18:10:35 +0300

    Auth Controller Register

---
+++ SehirRehberi.API/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SehirRehberi.API.Data;
+using SehirRehberi.API.Dtos;
+using SehirRehberi.API.Models;
+
+namespace SehirRehberi.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private IAuthRepository _authRepository;
+        IConfiguration _configuration;
+
+        public AuthController(IAuthRepository authRepository , IConfiguration configuration)
+        {
+            _authRepository = authRepository;
+            _configuration = configuration;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromBody] UserForRegisterDto userForRegisterDto)
+        {
+            if(await _authRepository.UserExist(userForRegisterDto.UserName))
+            {
+                ModelState.AddModelError("UserName", "User name already exist");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var userToCreate = new User
+            {
+                UserName = userForRegisterDto.UserName
+
+            };
+
+            var createdUser = await _authRepository.Register(userToCreate, userForRegisterDto.Password);
+            return StatusCode(201);
+        }
+
+    }
+}
